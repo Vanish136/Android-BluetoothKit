76bea608d0d576a05561f33552172d286cad3e7e
luowenkang <505515031@qq.com>
2019-11-21 09:56:00 +0800

    删除sample

--- lib_bluetoothkit/src/main/java/com/lwkandroid/bluetoothkit/utils/BluetoothLog.java
+++ lib_bluetoothkit/src/main/java/com/lwkandroid/bluetoothkit/utils/BluetoothLog.java
@@ -9,43 +9,53 @@ import java.io.Writer;
 /**
  * Created by dingjikerbo on 2015/12/16.
  */
-public class BluetoothLog {
+public class BluetoothLog
+{
 
-    private static final String LOG_TAG = "miio-bluetooth";
+    private static final String LOG_TAG = "BluetoothKit";
 
-    public static void i(String msg) {
+    public static void i(String msg)
+    {
         Log.i(LOG_TAG, msg);
     }
 
-    public static void e(String msg) {
+    public static void e(String msg)
+    {
         Log.e(LOG_TAG, msg);
     }
 
-    public static void v(String msg) {
+    public static void v(String msg)
+    {
         Log.v(LOG_TAG, msg);
     }
 
-    public static void d(String msg) {
+    public static void d(String msg)
+    {
         Log.d(LOG_TAG, msg);
     }
 
-    public static void w(String msg) {
+    public static void w(String msg)
+    {
         Log.w(LOG_TAG, msg);
     }
 
-    public static void e(Throwable e) {
+    public static void e(Throwable e)
+    {
         e(getThrowableString(e));
     }
 
-    public static void w(Throwable e) {
+    public static void w(Throwable e)
+    {
         w(getThrowableString(e));
     }
 
-    private static String getThrowableString(Throwable e) {
+    private static String getThrowableString(Throwable e)
+    {
         Writer writer = new StringWriter();
         PrintWriter printWriter = new PrintWriter(writer);
 
-        while (e != null) {
+        while (e != null)
+        {
             e.printStackTrace(printWriter);
             e = e.getCause();
         }
